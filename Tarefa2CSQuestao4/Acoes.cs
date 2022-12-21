d01ba17fa22a10a1c34509e999e6a91d732b24c5
Marcus Vinícius <marcusvinicius.paz2001@gmail.com>
2022-12-21 14:19:06 -0300

    Adicionar arquivos de projeto.

---
+++ Tarefa2CSQuestao4/Acoes.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarefa2CSQuestao4
+{
+    public class Acoes
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(funcaoRetornaQuantitadePOV(0.1M, 90)); //10
+            Console.WriteLine(funcaoRetornaQuantitadePOV(0.1M, 100)); //11
+            Console.WriteLine(funcaoRetornaQuantitadePOV(0.2M, 70)); //17
+        }
+        public static int funcaoRetornaQuantitadePOV(decimal porcentagem, int totalNegociado)
+        {
+            return (int) (porcentagem * totalNegociado / (1 - porcentagem));
+        }
+    }
+}
